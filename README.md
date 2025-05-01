154dcb4e8ef2b4eb94b607c0c91a97ea9455556d
rafaelsouzars <rafael21.santos@hotmailcom>
2025-05-01 15:19:08 -0300

    :recycle: refact(scanner): Organização e otimização do código do scanner
    Identação, comentarios e otimização de funcionalidades.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Parse JS
+# Parser JS
 
-![Static Badge](https://img.shields.io/badge/version-1.0.0-green) ![Static Badge](https://img.shields.io/badge/status-development-yellow)
+![Static Badge](https://img.shields.io/badge/version-1.1.0-green) ![Static Badge](https://img.shields.io/badge/status-development-yellow)
 
-Mini interpretador de linguagem de programação baseado no artigo [Seja um compilador - crie um compilador Javascript](https://medium.com/@henriquesosa/seja-um-compilador-crie-um-compilador-com-javascript-33a9557c8cdf)
+Mini interpretador de linguagem de programação baseado no artigo [Seja um compilador - crie um compilador Javascript](https://medium.com/@henriquesosa/seja-um-compilador-crie-um-compilador-com-javascript-33a9557c8cdf) 
--- parserjs.js
+++ parserjs.js
@@ -6,27 +6,32 @@
 	Author: Rafael Souza
 	Github: https://github.com/rafaelsouzars/mini-parsejs.git
 */
+
+// Lexemas
 const LEXERS = {
   // Palavras reservadas
   "reserved_words": [
-    "const",
-    "function",
-    "if",
-    "else",
-    "while",
-    "do",
-    "switch",
-    "case",
-    "var",
+	"const",
+	"function",
+	"if",
+	"else",
+	"while",
+	"do",
+	"switch",
+	"case",
+	"var",
 	"let",
-    "class",
-    "extends",
-    "abstract",
-    "static",
+	"class",
+	"extends",
+	"abstract",
+	"static",
 	"return",
 	"try",
 	"catch",
-	"finally"
+	"finally",
+	"true",
+	"false",
+	"array"
   ],
   // Numeros
   "numbers": [
@@ -43,13 +48,13 @@ const LEXERS = {
   ],
   "operators": {
 	"op_atr": "=",
-    "op_adc": "+",
-    "op_sub": "-",
-    "op_mul": "*",
-    "op_div": "/",
+	"op_adc": "+",
+	"op_sub": "-",
+	"op_mul": "*",
+	"op_div": "/",
 	"op_equ": "==",
-    "op_maj": ">",
-    "op_min": "<",	
+	"op_maj": ">",
+	"op_min": "<",	
 	"op_mje": ">=",
 	"op_mne": "<=",
 	"op_nte": "!=",
@@ -57,27 +62,24 @@ const LEXERS = {
   },
   // Delimitadores
   "delimiters": {
-    "left_parent": "\(",
-    "right_parent": "\)",
-    "left_square": "\[",
-    "right_square": "\]",
-    "left_curly": "\{",
-    "right_curly": "\}",
-    "comma": ",",
-    "dot": ".",
-    "colon": ":",
-    "semicolon": ";",
-    "quotation": "\"",
-	"white_space": " "
+	"left_parent": "\(",
+	"right_parent": "\)",
+	"left_square": "\[",
+	"right_square": "\]",
+	"left_curly": "\{",
+	"right_curly": "\}",
+	"comma": ",",
+	"dot": ".",
+	"colon": ":",
+	"semicolon": ";",
+	"quotation": "\"",
+	"white_space": " ",
+	"new_line": "\n"
 	/*"empty": ""*/
   }
 }
 
-/*for(const [key, value] of Object.entries(LEXERS)){
-  console.log(key, value)
-}*/
-
-// Constantes globais
+// Array constantes globais para manipulação dos lexemas
 const numbers = Object.entries(
 						LEXERS['numbers']
 					).map(
@@ -101,121 +103,132 @@ const operators = Object.entries(
 						}
 					);
 					
-const inputString = `let str == "ola"function test(){if(2.2==2.2){return true}}`; // Código de entrada
-const lexers = []; // Array para armazenar lexemas
+const inputString = `let array arr = []
+arr = {0,1,2,3,4}` // Código de entrada
+const lexers = [] // Array para armazenar lexemas
 
 // Variáveis globais
-let token = [];
-let charBuffer = new String();
-let readLiteralState = false; //console.log('Initial state: ', readLiteralState)
+let token = []
+let charBuffer = new String() // Buffer de caracteres
+let readLiteralState = false // Flag para indicar leitura de literal
 
 // Itera os caracteres do código de entrada
-for (const [id,char] of Object.entries(inputString)) {//isNaN(Number(char))
-  // Verifica se o caracter é alfabético
-  if (/[a-zA-Z]/gm.test(char)) {	  
-	// Se o buffer não contém nenhuma palavra reservada então continua-se adicionando caracteres
-	if (!LEXERS['reserved_words'].includes(charBuffer)&&!readLiteralState) {
-		// Verifica se o buffer contem um number para inserir no array de lexemas
-		if (!isNaN(Number(charBuffer))&&charBuffer.length>0) {				
+for (const [id,char] of Object.entries(inputString)) {
+	
+	// Testa os caracteres alfabéticos
+	if (/[a-zA-Z]/gm.test(char)) {		  
+		// Se o buffer não contém nenhuma palavra reservada então continua-se adicionando caracteres
+		if (!LEXERS['reserved_words'].includes(charBuffer)&&!readLiteralState) {
+			//	
+			charBuffer += char		
+		}		
+		else if (LEXERS['reserved_words'].includes(charBuffer)&&!readLiteralState) {
+			//		
+			lexers.push(charBuffer)			
+			charBuffer = ''	
+		}
+		else if (readLiteralState) {
+			lexers[lexers.length-1] += char
+		} 	
+	}
+	  
+	// Testa os operadores e delimitadores
+	else if (/^[\)\(\{\}\[\]\.\:\;\,\s\n\t\=\+\-\*\%\|\!\<\>\"\'\\]/gm.test(char)) {	
+		// Testa o caractere '.' para analisar se faz parte de um numero real	
+		if (/\./.test(char)) {//
 			lexers.push(charBuffer)
 			charBuffer = ''
-		}		
-		charBuffer += char		
-	}		
-	else if (LEXERS['reserved_words'].includes(charBuffer)&&!readLiteralState) {
-		// 
-		lexers.push(charBuffer)			
-		charBuffer = ''	
-	}
-	else if (readLiteralState) {
-		lexers[lexers.length-1] += char
-	} 	
-  }
-  else if (/^[\)\(\{\}\[\]\.\:\;\,\s\=\+\-\*\%\|\!\<\>\"\'\\]/gm.test(char)) {	
-	//console.log(charBuffer)
-	if (/\./.test(char)) {//
-		lexers.push(charBuffer)
-		charBuffer = ''
-		if (!isNaN(Number(lexers[lexers.length-1]))) {			
-			//console.log("dot: ", char, " lexer-1: ", lexers[lexers.length-1])
+			if (!isNaN(Number(lexers[lexers.length-1]))) {			
+				lexers[lexers.length-1] += char
+			}			
+		}
+		// Testa o caractere 'igual' para analisar se é uma igualdade ou atribuição
+		else if (/\=/.test(char)&&/^[\=\>\<\!]/.test(lexers[lexers.length-1])) {
 			lexers[lexers.length-1] += char
 		}
-		
-	}
-	else if (/\=/.test(char)&&/^[\=\>\<\!]/.test(lexers[lexers.length-1])) {
-		lexers[lexers.length-1] += char
-	}
-	else if (/^[\"\']/.test(char)) {
-		if (!readLiteralState) {			
-			lexers.push(charBuffer)		
-			lexers.push(char)
-			charBuffer = ''
-			readLiteralState = !readLiteralState
-			//console.log('Read state: ', readLiteralState)
+		// Testa os caracteres de aspas para identificar e armazenar uma constante literal
+		else if (/^[\"\']/.test(char)) {
+			if (!readLiteralState) {
+				if (charBuffer.length > 0) {
+					lexers.push(charBuffer)
+					charBuffer = ''
+				}					
+				lexers.push(char)			
+				readLiteralState = !readLiteralState			
+			}
+			else {
+				lexers[lexers.length-1] += char			
+				readLiteralState = !readLiteralState
+			}
+			
 		}
-		else {
-			lexers[lexers.length-1] += char			
-			readLiteralState = !readLiteralState
-		}
-		
-	}
-	else {
-		lexers.push(charBuffer)		
-		lexers.push(char)
-		charBuffer = ''
-	}
-		 
-  }  
-  else if (/^[0-9]/gm.test(char)) { 		
-		// Verifica se o caracter é numeral		
-		if (numbers.includes(Number(char))){
-			// Verifica se o buffer contem uma palavra reservada ou identificador para inserir no array de lexemas
-						
-			// Testa se o ultimo elemento do array de lexemas é um literal_number
-			if (!isNaN(Number(lexers[lexers.length-1]))&&/\./gm.test(lexers[lexers.length-1])) {
+		// Testa um espaço em branco e adiciona regras para armazenamento e limpeza do buffer de leitura
+		else if(/\s/.test(char)) {		
+			if (readLiteralState) {
 				lexers[lexers.length-1] += char
-				//console.log("float: ", lexers[lexers.length-1])				
+			}
+			else {
+				if (charBuffer.length > 0) {
+					lexers.push(charBuffer)
+					charBuffer = ''
+				}			
+			}				
+		}
+		// Testa se não é uma nova linha ou tabulação
+		else if (!/^[\n\t]/.test(char)&&char!==''){		
+			if (charBuffer.length > 0) {
+				lexers.push(charBuffer)
+				charBuffer = ''
+			}		
+			lexers.push(char)
+		}		 
+	}
+	  
+	// Testa os caracteres numericos
+	else if (/^[0-9]/gm.test(char)) { 		
+		// Verifica se o caracter é numeral		
+		if (numbers.includes(Number(char))){						
+			// Testa se o ultimo elemento do array de lexemas é um number
+			if (!isNaN(Number(lexers[lexers.length-1]))&&/\./gm.test(lexers[lexers.length-1])) {
+				lexers[lexers.length-1] += char							
 			} else {
 				charBuffer += char
 				//lexers.push(char)
-			} 
-			
+			}		
 		}				
-  }  
+	}	
+
 }
 
+// Filtra e mapea o array de lexemas e gera o array de tokens
 tokens = lexers.filter(
-			whiteSpace => {
-				return (whiteSpace!=" "&&whiteSpace!="")
-			}
-		).map(
-			(lexer,index) => {
-			  if(LEXERS['reserved_words'].includes(lexer)){
-				//return {'reserved_word': v}
-				//return JSON.parse(`{"rw_${lexer}" : "${lexer}"}`)
-				return JSON.parse(`{"${lexer}" : "${lexer}"}`)
-			  } 
-			  else if(delimiters.includes(lexer)){
-				return {"delimiter": lexer}
-			  } 
-			  else if (operators.includes(lexer)){
-				let token
-				Object.entries(LEXERS['operators']).forEach(([key,value])=>{
-					if(lexer==value) token = key
-				  })
-				return JSON.parse(`{ "${lexer}" : "${lexer}"}`)
-			  } 
-			  else if(/^\d/.test(Number(lexer))){
-				return {"number": lexer}
-			  }
-			  else if (/[\"{,2}]|[\'{,2}]/.test(lexer)) {
-				return {"literal": lexer}  
-			  }
-			  else {
-				return {"id": lexer}
-			  }
-			}
-);
+				ignoreChars => {
+					return (ignoreChars!=" "&&ignoreChars!=""&&ignoreChars!="\n"&&ignoreChars!="\t")
+				}
+			).map(
+					(lexer,index) => {
+						// Gera o token para palavras reservadas
+						if(LEXERS['reserved_words'].includes(lexer)){				
+							return JSON.parse(`{"${lexer}": "${lexer}"}`)
+						}
+						// Gera o token para delimitadores
+						else if(delimiters.includes(lexer)||operators.includes(lexer)){
+							return JSON.parse(`{"${lexer}": "${lexer}"}`)
+						}
+						// Gera o token para numeros
+						else if(/^\d/.test(Number(lexer))){
+							return {"number": lexer}
+						}
+						// Gera o token para tipo literal
+						else if (/^[\"{,2}]|^[\'{,2}]/.test(lexer)) {
+							return {"literal": lexer}  
+						}
+						// Gera o token para identificadores
+						else {
+							return {"id": lexer}
+						}
+					}
+				);
 
 // Entrada
 console.log("Input: ", inputString)
