cb6d7e26174d120bd3180e4b960d77a563ed1266
rafaelsouzars <rafael21.santos@hotmailcom>
2025-05-02 22:57:27 -0300

    :bug: fix:Correções no analisador léxico
    Remoção de bugs e correções no analisador léxico

--- parserjs.js
+++ parserjs.js
@@ -10,7 +10,7 @@
 // Lexemas
 const LEXERS = {
   // Palavras reservadas
-  "reserved_words": [
+  "keywords": [
 	"const",
 	"function",
 	"if",
@@ -62,6 +62,9 @@ const LEXERS = {
 	"===",
 	"+=",
 	"-=",
+	"++",
+	"--",
+	"=>",
 	"@",
 	"?",
 	"#",
@@ -91,8 +94,8 @@ const LEXERS = {
 	/*"empty": ""*/
   ]
 }
-					
-const inputCode = `function teste () {return \$\`{teste}\`}` // Código de entrada
+				
+const inputCode = `let func = (s)=>{return "hello world"}` // Código de entrada
 const lexers = [] // Array para armazenar lexemas
 
 // Variáveis globais
@@ -101,162 +104,133 @@ let charBuffer = new String() // Buffer de caracteres
 let readLiteralState = false // Flag para indicar leitura de literal
 let readCommentState = false
 
-// Funções
-function lexSearchList(obj, searchBuffer) {
-	return obj.map(
-				(ele, idx) => {
-						if (ele.indexOf(searchBuffer, 0)==0) {
-							//console.log('ele: ', ele , ' substring: ' ,ele.substring(0, charBuffer.length) , ' index: ', ele.substring(0, charBuffer.length).indexOf(charBuffer, 0))
-							return ele
-						}
-					}).filter(
-						(arr) => {
-							//console.log('arr: ',arr)
-							return (arr!=undefined)
-						}) 
-}
-
 // Itera os caracteres do código de entrada
-for (const [index,character] of Object.entries(inputCode)) {
-	
+for (const [key, character] of Object.entries(inputCode)) {
+    
+	charBuffer += character	
 	// Testa os caracteres alfabéticos
-	if (/[a-zA-Z]/gm.test(character)) {		  
-		// Se o buffer não contém nenhuma palavra reservada então continua-se adicionando caracteres
-		if (!LEXERS['reserved_words'].includes(charBuffer)) {
-			//	
-			charBuffer += character
-			//console.log(lexSearchList(LEXERS['reserve_words'], charBuffer))		
-		}		
-		else if (LEXERS['reserved_words'].includes(charBuffer)&&!readLiteralState) {
-			//		
+	if (/^[a-zA-Z]/.test(charBuffer)) {
+	  if (LEXERS['keywords'].includes(charBuffer)&&!readLiteralState) {
 			lexers.push(charBuffer)			
 			charBuffer = ''	
-		}			
+	  }
 	}
-	  
-	// Testa os operadores e delimitadores
-	if (/^[\)\(\{\}\[\]\.\:\;\,\@\#\$\?\s\n\t\=\+\-\*\%\|\!\<\>\`\"\'\/\\]/gm.test(character)) {	
-		// Testa o caractere '.' para analisar se faz parte de um numero real		
-		if (/\./.test(character)) {//
-			lexers.push(charBuffer)
+
+	if (/^[\)\(\{\}\[\]\:\;\,\@\#\$\?\s\n\t\=\+\-\*\%\|\!\<\>\`\"\'\/\\]/gm.test(charBuffer[charBuffer.length-1])) {
+	  // Testa os operadores e delimitadores
+	  if (/^[\s]/gm.test(charBuffer[charBuffer.length-1])) {
+		if (!readLiteralState) {
+			lexers.push(charBuffer.substring(0,charBuffer.length-1))
 			charBuffer = ''
-			if (!isNaN(Number(lexers[lexers.length-1]))) {			
-				lexers[lexers.length-1] += character
-			}			
-		}
-		// Testa o caractere 'igual' para analisar se é uma igualdade ou atribuição
-		else if (/\=/.test(character)&&/^[\=\>\<\!\+\-]/.test(lexers[lexers.length-1])) {			
-			lexers[lexers.length-1] += character			
-		}
-		// Testa o caractere 'igual' para analisar se é uma igualdade ou atribuição
-		else if (((/\//.test(character)||/\*/.test(character))&&/\//.test(lexers[lexers.length-1]))||(/\//.test(character)&&/\*/.test(lexers[lexers.length-1]))) {			
-			lexers[lexers.length-1] += character
-			if (lexers[lexers.length-1]=="\/\/") {
-				readCommentState = !readCommentState
-				console.log(readCommentState)
-			}			
 		}		
-		// Testa os caracteres de aspas para identificar e armazenar uma constante literal
-		else if (/^[\"\']/.test(character)) {
+	  }
+	  // Testa o caractere 'igual' para analisar se é uma igualdade ou atribuição
+	  else if (/\=/.test(charBuffer[charBuffer.length-1])&&/^[\=\>\<\!\+\-]/.test(lexers[lexers.length-1])) {			
+			  lexers[lexers.length-1] += charBuffer[charBuffer.length-1]
+			  charBuffer = ''
+		  }
+		  // Testa os sinais de indremento e decremento
+	  else if ((/\+/.test(charBuffer[charBuffer.length-1])&&/\+/.test(lexers[lexers.length-1]))||(/\-/.test(charBuffer[charBuffer.length-1])&&/\-/.test(lexers[lexers.length-1]))) {			
+			  lexers[lexers.length-1] += charBuffer[charBuffer.length-1]
+			  charBuffer = ''
+		  }
+		  // Testa o sinal arrow
+	  else if (/\>/.test(charBuffer[charBuffer.length-1])&&/\=/.test(lexers[lexers.length-1])) {			
+			  lexers[lexers.length-1] += charBuffer[charBuffer.length-1]
+			  charBuffer = ''
+		  }
+		  // Testa os caracteres de aspas para identificar e armazenar uma constante literal
+		  else if (/^[\"\'\``]/.test(charBuffer[charBuffer.length-1])) {
 			if (!readLiteralState) {
-				if (charBuffer.length > 0) {
-					lexers.push(charBuffer)
+				if (charBuffer.length > 1) {
+					lexers.push(charBuffer.substring(0,charBuffer.length-1).trim())
+					lexers.push(charBuffer[charBuffer.length-1])
 					charBuffer = ''
-				}					
-				lexers.push(character)			
-				readLiteralState = !readLiteralState			
+				}
+				else {
+				  lexers.push(charBuffer[charBuffer.length-1])
+					charBuffer = ''
+				}
+				readLiteralState = !readLiteralState // Muda estado da flag para true			
 			}
 			else {
-				//lexers[lexers.length-1] += character
-				charBuffer += character
-				lexers[lexers.length-1] += charBuffer
-				charBuffer = ''
+				if (charBuffer.length > 0) {
+					lexers[lexers.length-1] += charBuffer
+					charBuffer = ''
+				}
+				else {
+				  lexers[lexers.length-1] += charBuffer
+					charBuffer = ''
+				}
 				readLiteralState = !readLiteralState
 			}
 			
-		}
-		// Testa um espaço em branco e adiciona regras para armazenamento e limpeza do buffer de leitura
-		else if(/\s/.test(character)) {		
-			if (readLiteralState) {
-				//lexers[lexers.length-1] += character
-				charBuffer += character
-			}
-			else {
-				if (charBuffer.length > 0) {
-					lexers.push(charBuffer)
-					charBuffer = ''
-				}			
-			}				
-		}
-		// Testa se não é uma nova linha ou tabulação
-		else if (!/^[\n\t]/.test(character)&&character!=='') {		
-			if (charBuffer.length > 0) {
-				lexers.push(charBuffer)
-				charBuffer = ''				
-			}		
-			lexers.push(character)
-			//readCommentState = !readCommentState
-			//console.log(readCommentState)
-		}
-		
-	}
-	  
-	// Testa os caracteres numericos
-	if (/^[0-9]/gm.test(character)) { 		
-		// Verifica se o caracter é numeral		
-		if (LEXERS['numbers'].includes(Number(character))) {						
-			// Testa se o ultimo elemento do array de lexemas é um number
-			if (!isNaN(Number(lexers[lexers.length-1]))&&/\./gm.test(lexers[lexers.length-1])) {
-				lexers[lexers.length-1] += character							
-			} else {
-				charBuffer += character	
-				//console.log('number session 1: ', charBuffer)
-			}		
-		}
-		else {
-			//console.log('number session 2: ', charBuffer)
-		} 				
-	}
-	// Testa o final do inputCode
-	if (Object.entries(inputCode).length-1==index)  {
-		if (charBuffer!=''&&charBuffer!=' ') {
-			lexers.push(charBuffer)
+		  }
+		  // Testa o delimitador de comentario
+		else if (/\/\//gm.test(charBuffer)) {	
 			charBuffer = ''
+			readCommentState = !readCommentState
+			console.log(readCommentState)
+		}
+		// Testa se o caracter de nova linha para encerra o comentario de uma linha
+		else if (/\n/.test(charBuffer[charBuffer.length-1])) {
+		  if (readCommentState) {
+			readCommentState = !readCommentState
+			console.log(readCommentState)
+		  }
 		}
 		
+	  else if (/[\(\)\{\}\[\]\:\;\,\@\#\$\?\s\t\%\|\\\+\-\<\>\*\!\=]/.test(charBuffer[charBuffer.length-1])) {
+		lexers.push(charBuffer.substring(0,charBuffer.length-1))
+		lexers.push(charBuffer[charBuffer.length-1])
+		charBuffer = ''
+	  }
+	  
+	}
+	// Testa os caracteres numéricos (obs: futuramente haverá uma nova implementação)
+	if (/^[0-9]/gm.test(charBuffer)) {
+	  /*if (/^[-+]?\d+(\.\d+)?$/.test(charBuffer)) {
+		
+	  }*/
+	}
+	//Testa o final do inputCode
+	if ((Object.entries(inputCode).length-1==key)) {
+	  if (charBuffer.length>0) {
+		lexers.push(charBuffer)
+	  }
 	}
 }
-
+  
 // Filtra e mapea o array de lexemas e gera o array de tokens
 tokens = lexers.filter(
-				ignoreChars => {
-					return (ignoreChars!=" "&&ignoreChars!=""&&ignoreChars!="\n"&&ignoreChars!="\t")
-				}
-			).map(
-					(lexer,index) => {
-						// Gera o token para palavras reservadas
-						if(LEXERS['reserved_words'].includes(lexer)){				
-							return JSON.parse(`{"${lexer}": "${lexer}"}`)
-						}
-						// Gera o token para delimitadores
-						else if(LEXERS['delimiters'].includes(lexer)||LEXERS['operators'].includes(lexer)){
-							return JSON.parse(`{"${lexer}": "${lexer}"}`)
-						}
-						// Gera o token para numeros
-						else if(/^\d/.test(Number(lexer))){
-							return {"number": lexer}
-						}
-						// Gera o token para tipo literal
-						else if (/^[\"{,2}]|^[\'{,2}]/.test(lexer)) {
-							return {"literal": lexer}  
-						}
-						// Gera o token para identificadores
-						else {
-							return {"id": lexer}
-						}
-					}
-				);
-
+        				ignoreChars => {
+        					return (ignoreChars!=" "&&ignoreChars!=""&&ignoreChars!="\n"&&ignoreChars!="\t")
+        				}
+        			).map(
+        					(lexer,index) => {
+        						// Gera o token para palavras reservadas
+        						if(LEXERS['keywords'].includes(lexer)){				
+        							return JSON.parse(`{"${lexer}": "${lexer}"}`)
+        						}
+        						// Gera o token para delimitadores
+        						else if(LEXERS['delimiters'].includes(lexer)||LEXERS['operators'].includes(lexer)){
+        							return JSON.parse(`{"${lexer}": "${lexer}"}`)
+        						}
+        						// Gera o token para numeros
+        						else if(/^\d/.test(Number(lexer))){
+        							return {"number": lexer}
+        						}
+        						// Gera o token para tipo literal
+        						else if (/^[\"{,2}]|^[\'{,2}]|^[\`{,2}]/.test(lexer)) {
+        							return {"literal": lexer}  
+        						}
+        						// Gera o token para identificadores
+        						else {
+        							return {"id": lexer}
+        						}
+        					}
+        				);
+console.log('saida')
 // Entrada
 console.log("Input: ", inputCode)
 // Lexemas
